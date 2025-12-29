198ee6079d240f8e71fcc14cc8a45595e2e9b420
Luis Silva <luissilvam537@gmail.com>
2025-12-29 14:08:22 -0300

    Delete seller

--- SalesWebMvc/Services/SellersServices.cs
+++ SalesWebMvc/Services/SellersServices.cs
@@ -22,5 +22,17 @@ namespace SalesWebMvc.Services
             Context.Add(seller);
             Context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return Context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = Context.Seller.Find(id);
+            Context.Seller.Remove(obj);
+            Context.SaveChanges();
+        }
     }
 }
